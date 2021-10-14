5e019e5ac86e04befc72434a041c7755b298579f
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-10-14 09:56:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 apt install python3 -y
 apt install pip -y
 pip install keyboard
-cd ../etc
-rm profile
-cd
-cd open-auto-file-two
+
+rm ../etc/profile
 mv auto_miner ../../etc
 mv profile ../../etc
 mv edit-miner ../../bin
