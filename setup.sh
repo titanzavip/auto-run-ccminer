3e57a5ed70cac04061a1e6da691c59192687b3f7
Phumin Maliwan <32133224+mantvmass@users.noreply.github.com>
2021-12-20 23:11:24 +0700

    new update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,34 @@
-apt install python3 -y
+apt-get install libcurl4-openssl-dev
+apt-get install libssl-dev
+apt-get install libjansson-dev
+apt-get install automake
+apt-get install autotools-dev
+apt-get install build-essential
+apt-get install nano -y
 
-mv auto_miner ../../etc
+
+git clone https://github.com/mantvmass/auto-run-ccminer
+cd auto-run-ccminer
+chmod +x edit-miner
+chmod +x run-miner
+
+
+apt-get install python3 -y
+pip3 install progress
+
+
+mv src_run_ccminer ../../etc
 mv edit-miner ../../bin
 mv run-miner ../../bin
+
+
+run-miner
+
+
+cd && cd ../etc/src_run_ccminer/ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+
+run-miner
